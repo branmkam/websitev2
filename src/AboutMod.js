071d5c93982559b0d143fa52d0273e7fce45276f
branmkam <54331071+branmkam@users.noreply.github.com>
2020-12-04 11:35:17 -0500

    link try again with pdf

--- src/AboutMod.js
+++ src/AboutMod.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { DownloadLink } from 'react-download-link';
+import { Link } from 'react-router-dom';
 import myheadshot from './images/myHeadshot.jpg';
 function AboutMod() {
 
@@ -21,10 +21,7 @@ function AboutMod() {
          <p>To see my computer science projects, click on the "My Projects" tab!</p>
     <br />
       <h2>Experience</h2>
-      <h2><DownloadLink
-      label="Download my resume! (.docx)"
-      filename="/files/ResumeBrandonKaminski.docx"
-    /> | <a href = "https://www.linkedin.com/in/brandon-kaminski-86580a19a/" target = "_blank">View my LinkedIn profile!</a></h2>
+      <h2><Link to="/files/ResumeBrandonKaminski.pdf" target="_blank" download>Download</Link> | <a href = "https://www.linkedin.com/in/brandon-kaminski-86580a19a/" target = "_blank">View my LinkedIn profile!</a></h2>
       <p></p>
    </div>
   </div>
